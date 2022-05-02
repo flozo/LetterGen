4aaa39c5b4abbdf73e82cbf82224f03e3d92f2c9
flozo.home <github.mail@flozo.de>
2022-05-02 16:50:17 +0200

    Update ArgumentListTest class

--- LetterGen/src/de/flozo/latex/ArgumentList.java
+++ LetterGen/src/de/flozo/latex/ArgumentList.java
@@ -4,13 +4,19 @@ import java.util.List;
 
 public class ArgumentList extends ExpressionList {
 
+    public static final StatementTerminator DEFAULT_TERMINATOR = StatementTerminator.COMMA;
+    public static final Bracket DEFAULT_BRACKETS = Bracket.SQUARE_BRACKETS;
+    public static final boolean DEFAULT_SKIP_LAST = true;
+    public static final boolean DEFAULT_INPLACE = false;
+
+
     public ArgumentList(String... lines) {
         super(lines);
     }
 
     @Override
     public List<String> getLines() {
-        return super.getLines(StatementTerminator.COMMA, false, Bracket.SQUARE_BRACKETS, false);
+        return super.getLines(DEFAULT_TERMINATOR, DEFAULT_SKIP_LAST, DEFAULT_BRACKETS, DEFAULT_INPLACE);
     }
 
     @Override
--- LetterGen/test/de/flozo/latex/ArgumentListTest.java
+++ LetterGen/test/de/flozo/latex/ArgumentListTest.java
@@ -1,6 +1,17 @@
 package de.flozo.latex;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ArgumentListTest {
+
+    public static final StatementTerminator DEFAULT_TERMINATOR = ArgumentList.DEFAULT_TERMINATOR;
+    public static final Bracket DEFAULT_BRACKETS = ArgumentList.DEFAULT_BRACKETS;
+    public static final String INLINE_SEPARATOR = ExpressionList.INLINE_SEPARATOR;
 
     private final String[] arguments = {
             "anchor=north west",
@@ -10,7 +21,30 @@ class ArgumentListTest {
             "align=left"
     };
 
+    @Test
+    void getLines() {
+        List<String> expected = new ArrayList<>(List.of(
+                DEFAULT_BRACKETS.getLeftBracket(),
+                "anchor=north west" + DEFAULT_TERMINATOR.getString(),
+                "minimum width=9.0cm" + DEFAULT_TERMINATOR.getString(),
+                "minimum height=2.73cm" + DEFAULT_TERMINATOR.getString(),
+                "text width=9.0cm" + DEFAULT_TERMINATOR.getString(),
+                "align=left",
+                DEFAULT_BRACKETS.getRightBracket()
+        ));
+        ArgumentList argumentList = new ArgumentList(arguments);
+        assertEquals(expected, argumentList.getLines());
+    }
 
+    @Test
+    void getInline() {
+        String expected = String.format(DEFAULT_BRACKETS.getLeftBracket() +
+                        "anchor=north west,%1$sminimum width=9.0cm,%1$s" +
+                        "minimum height=2.73cm,%1$stext width=9.0cm,%1$salign=left" +
+                        DEFAULT_BRACKETS.getRightBracket(),
+                INLINE_SEPARATOR);
+        ExpressionList expressionList = new ExpressionList(arguments);
+        assertEquals(expected, expressionList.getInline(DEFAULT_TERMINATOR, DEFAULT_BRACKETS));
 
-
+    }
 }
