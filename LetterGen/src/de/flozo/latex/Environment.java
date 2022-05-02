849c0a75cea0f19d5553252623075fa258eb9a09
flozo.home <github.mail@flozo.de>
2022-05-02 17:51:36 +0200

    Add EnvironmentTest class

--- LetterGen/src/de/flozo/latex/Environment.java
+++ LetterGen/src/de/flozo/latex/Environment.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 public class Environment {
 
-    private static final String OPENING_KEYWORD = "begin";
-    private static final String CLOSING_KEYWORD = "end";
+    public static final String OPENING_KEYWORD = "begin";
+    public static final String CLOSING_KEYWORD = "end";
     public static final String INDENT_CHARACTER = "\t";
+    public static final Bracket DEFAULT_OPTION_BRACKET = ArgumentList.DEFAULT_BRACKETS;
 
     private final EnvironmentName name;
     //    private final String argument;
@@ -16,22 +17,24 @@ public class Environment {
     private final ExpressionList body;
     private final List<String> lines;
 
+    private final String openingTag;
+    private final String closingTag;
+
     public Environment(EnvironmentName name, ArgumentList optionalParameters, boolean inlineOptions, ExpressionList body) {
         this.name = name;
         this.optionalParameters = optionalParameters;
         this.inlineOptions = inlineOptions;
         this.body = body;
         this.lines = new ArrayList<>();
+        this.openingTag = "\\" + OPENING_KEYWORD + "{" + name.getString() + "}";
+        this.closingTag = "\\" + CLOSING_KEYWORD + "{" + name.getString() + "}";
     }
 
-
     public List<String> getLines() {
-        String openingTag = "\\" + OPENING_KEYWORD + "{" + name + "}";
-        String closingTag = "\\" + CLOSING_KEYWORD + "{" + name + "}";
         if (inlineOptions) {
             lines.add(openingTag + " " + optionalParameters.getInline());
         } else {
-            lines.add(openingTag + " [");
+            lines.add(openingTag + " " + DEFAULT_OPTION_BRACKET.getLeftBracket());
             lines.addAll(indent(optionalParameters.getLines().subList(1, optionalParameters.len() + 2)));
         }
         lines.addAll(indent(body.getLines()));
--- LetterGen/test/de/flozo/latex/EnvironmentTest.java
+++ LetterGen/test/de/flozo/latex/EnvironmentTest.java
@@ -1,21 +1,81 @@
 package de.flozo.latex;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class EnvironmentTest {
 
-//    private Environment environment = new Environment();
-//
-//    @Test
-//    void assembleEnvironment() {
-//        String environmentName = "pgfonlayer";
-//        String environmentArgument = "background";
-//        List<String> codeBlock = List.of("\\fill [fill=none] (0, 0) rectangle (21.0, 29.7);");
-//
-//        List<String> expectedResult = List.of(
-//                "\\begin{pgfonlayer}{background}",
-//                "\t\\fill [fill=none] (0, 0) rectangle (21.0, 29.7);",
-//                "\\end{pgfonlayer}"
-//        );
-//        assertEquals()
-//
-//    }
+    public static final String INLINE_SEPARATOR = ExpressionList.INLINE_SEPARATOR;
+
+    public static final String OPENING_KEYWORD = Environment.OPENING_KEYWORD;
+    public static final String CLOSING_KEYWORD = Environment.CLOSING_KEYWORD;
+
+    public static final String INDENT_CHARACTER = Environment.INDENT_CHARACTER;
+    public static final StatementTerminator TERMINATOR = StatementTerminator.COMMA;
+
+    private final String[] arguments = {
+            "anchor=north west",
+            "minimum width=9.0cm",
+            "minimum height=2.73cm",
+            "text width=9.0cm",
+            "align=left"
+    };
+
+    private final String[] body = {
+            "% Test line",
+            "% Another test line",
+            "% And another one"
+    };
+
+
+    @ParameterizedTest
+    @EnumSource(EnvironmentName.class)
+    void getLines_blockOptions(EnvironmentName environmentName) {
+        List<String> expected = new ArrayList<>(List.of(
+                "\\" + OPENING_KEYWORD + "{" + environmentName.getString() + "} [",
+                INDENT_CHARACTER + "anchor=north west" + TERMINATOR.getString(),
+                INDENT_CHARACTER + "minimum width=9.0cm" + TERMINATOR.getString(),
+                INDENT_CHARACTER + "minimum height=2.73cm" + TERMINATOR.getString(),
+                INDENT_CHARACTER + "text width=9.0cm" + TERMINATOR.getString(),
+                INDENT_CHARACTER + "align=left",
+                INDENT_CHARACTER + "]",
+                INDENT_CHARACTER + "% Test line",
+                INDENT_CHARACTER + "% Another test line",
+                INDENT_CHARACTER + "% And another one",
+                "\\" + CLOSING_KEYWORD + "{" + environmentName.getString() + "}"
+        ));
+        ArgumentList argumentList = new ArgumentList(arguments);
+        ExpressionList expressionList = new ExpressionList(body);
+        Environment environment = new Environment(environmentName, argumentList, false, expressionList);
+        assertEquals(expected, environment.getLines());
+    }
+
+    @ParameterizedTest
+    @EnumSource(EnvironmentName.class)
+    void getLines_inlineOptions(EnvironmentName environmentName) {
+        String expectedOptions = String.format(
+                "\\" + OPENING_KEYWORD + "{" + environmentName.getString() + "} [" +
+                "anchor=north west" + "%1$s%2$s" +
+                "minimum width=9.0cm" + "%1$s%2$s" +
+                "minimum height=2.73cm" + "%1$s%2$s" +
+                "text width=9.0cm" + "%1$s%2$s" +
+                "align=left]", TERMINATOR.getString(), INLINE_SEPARATOR);
+        List<String> expected = new ArrayList<>(List.of(
+                expectedOptions,
+                INDENT_CHARACTER + "% Test line",
+                INDENT_CHARACTER + "% Another test line",
+                INDENT_CHARACTER + "% And another one",
+                "\\" + CLOSING_KEYWORD + "{" + environmentName.getString() + "}"
+        ));
+        ArgumentList argumentList = new ArgumentList(arguments);
+        ExpressionList expressionList = new ExpressionList(body);
+        Environment environment = new Environment(environmentName, argumentList, true, expressionList);
+        assertEquals(expected, environment.getLines());
+    }
+
 }
