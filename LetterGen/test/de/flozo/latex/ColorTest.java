5eea5833ff77ea6e5d0254fb385053a9de35092d
flozo.home <github.mail@flozo.de>
2022-05-02 18:16:39 +0200

    Update ColorTest class using parametrized tests

--- LetterGen/test/de/flozo/latex/ColorTest.java
+++ LetterGen/test/de/flozo/latex/ColorTest.java
@@ -1,21 +1,33 @@
 package de.flozo.latex;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ColorTest {
 
-//    @Test
-//    void testToString_BluesK() {
-//        assertEquals("Blues-K", new Color(ColorScheme.BLUES, ColorLetter.K).getString());
-//    }
-//
-//    @Test
-//    void testToString_YlGnBu() {
-//        assertEquals("YlGnBu-C", new Color(ColorScheme.YL_GN_BU, ColorLetter.C).getString());
-//    }
-//
-//    @Test
-//    void testToString_BuGn() {
-//        assertEquals("BuGn-M", new Color(ColorScheme.BU_GN, ColorLetter.M).getString());
-//    }
-//
+    public static final ColorLetter TEST_COLOR_LETTER = ColorLetter.A;
+    public static final ColorScheme TEST_COLOR_SCHEME = ColorScheme.BLUES;
+    public static final String SEPARATOR = "-";
+
+    @ParameterizedTest
+    @EnumSource(ColorScheme.class)
+    void getString(ColorScheme colorScheme) {
+        Color color = new Color(colorScheme, TEST_COLOR_LETTER);
+        String expected = colorScheme.getString() + SEPARATOR + TEST_COLOR_LETTER;
+        System.out.println(color.getString());
+        assertEquals(expected, color.getString());
+    }
+
+    @ParameterizedTest
+    @EnumSource(ColorLetter.class)
+    void getString(ColorLetter colorLetter) {
+        Color color = new Color(TEST_COLOR_SCHEME, colorLetter);
+        String expected = TEST_COLOR_SCHEME.getString() + SEPARATOR + colorLetter;
+        System.out.println(color.getString());
+        assertEquals(expected, color.getString());
+    }
+
 
 }
