6520d06c1798d7e895f4fb9b94b07f79b67acace
flozo.home <github.mail@flozo.de>
2022-05-10 12:34:32 +0200

    New enum Alignment

---
+++ LetterGen/src/de/flozo/latex/tikz/Alignment.java
@@ -0,0 +1,23 @@
+package de.flozo.latex.tikz;
+
+public enum Alignment {
+    LEFT("left"),
+    RIGHT("right"),
+    CENTER("center"),
+    JUSTIFY("justify"),
+    FLUSH_LEFT("flush left"),
+    FLUSH_RIGHT("flush right"),
+    FLUSH_CENTER("flush center"),
+    NONE("none");
+
+    private final String alignment;
+
+    Alignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public String getString() {
+        return alignment;
+    }
+
+}
