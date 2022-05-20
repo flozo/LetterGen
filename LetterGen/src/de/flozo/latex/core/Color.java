01e86e589faea252628a5a9e25ef2c9951830beb
flozo.home <github.mail@flozo.de>
2022-05-20 19:48:56 +0200

    Make Color constructor also take StandardColorName

--- LetterGen/src/de/flozo/latex/core/Color.java
+++ LetterGen/src/de/flozo/latex/core/Color.java
@@ -2,21 +2,15 @@ package de.flozo.latex.core;
 
 public class Color {
 
-//    private final ColorScheme colorScheme;
-//    private final ColorLetter colorLetter;
     private final String name;
 
-
     public Color(ColorScheme colorScheme, ColorLetter colorLetter) {
-//        this.colorScheme = colorScheme;
-//        this.colorLetter = colorLetter;
         this.name = colorScheme.getString() + "-" + colorLetter.toString();
     }
 
     public Color(StandardColorName colorName) {
         this.name = colorName.getString();
     }
-
 
     public String getString() {
         return name;
