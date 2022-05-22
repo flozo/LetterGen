2e137d616f16e4cb95a3ea6c7250847f0171d3c2
flozo.home <github.mail@flozo.de>
2022-05-22 16:56:22 +0200

    Add custom Exception (LatexCompilationException)

---
+++ LetterGen/src/de/flozo/latex/LatexCompilationException.java
@@ -0,0 +1,8 @@
+package de.flozo.latex;
+
+public class LatexCompilationException extends Exception {
+
+    public LatexCompilationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
